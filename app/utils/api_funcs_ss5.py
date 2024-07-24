570211241269b3fd5b985b423ad66d50f9ccd69b
yotamtsabari <yotamtsa@gmail.com>
2024-07-24 19:55:24 +0300

    hi

--- app/utils/api_funcs_ss5.py
+++ app/utils/api_funcs_ss5.py
@@ -293,6 +293,28 @@ def get_batteries(ips):
     return result
 
 
+def get_ptt_groups(ips):
+    group_ips = [[str(i), f"239.0.0.{10 + i}"] for i in range(15)]
+    statuses = [[]]
+    max_group = 0
+    for radio_index, radio_ip in enumerate(ips):
+        ptt_groups = send_commands_ip(["ptt_active_mcast_group"], radio_ip=radio_ip, params=[])
+        states = ptt_groups.split('_')
+        listen = states[0].split(',')
+        talk = states[1].split(',')
+        monitor = states[2].split(',')
+        for i in range(15):
+            if i in listen or i in talk or i in monitor:
+                max_group = max(max_group, i)
+            if i in listen:
+                if i in talk:  # active
+                    statuses[radio_index].append(1)
+                elif i in monitor:  # monitor
+                    statuses[radio_index].append(2)
+                # should not get here
+            else:  # inactive or does not exist
+                statuses[radio_index].append(0)
+    return {'num_groups': max_group,'ips':ips, 'statuses': statuses}
 def set_ptt_groups(ips, num_groups, statuses):
     """
     This method sets ptt group settings for all radios
